0abf2662aa1f2db5f3a9703d346fbfde570d3702
Javlon Akhundjanov <zha.akhundzhanov@x5.ru>
2023-11-13 10:10:50 +0300

    add: typegoose

--- src/product/product.model/product.model.ts
+++ src/product/product.model/product.model.ts
@@ -1,17 +1,49 @@
-export class ProductModel {
-	id: string;
+import { prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
+class ProductCharacteristic {
+	@prop()
+	name: string;
+
+	@prop()
+	value: string;
+}
+
+export interface ProductModel extends Base {}
+export class ProductModel extends TimeStamps {
+	@prop()
 	image: string;
+
+	@prop()
 	title: string;
+
+	@prop()
 	price: number;
+
+	@prop()
 	oldPrice: number;
+
+	@prop()
 	loan: number;
+
+	@prop()
 	calculatedRating: number;
+
+	@prop()
 	description: string;
+
+	@prop()
 	advantages: string;
+
+	@prop()
 	disAvantages: string;
+
+	@prop({ type: () => [String] })
 	categories: string[];
+
+	@prop({ type: () => [String] })
 	tags: string[];
-	characteristics: {
-		[key: string]: string;
-	};
+
+	@prop({ type: () => [ProductCharacteristic], _id: false })
+	characteristics: ProductCharacteristic[];
 }
